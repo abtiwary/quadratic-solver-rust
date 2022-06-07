636f783e50004e73973dcf6f5f57cc92e154e425
abtiwary <ab.tiwary@gmail.com>
2022-06-08 00:16:20 +1000

    Implement FromStr for Quadratic
    Implemented  the `FromStr` trait for `Quadratic` in order to turbofish
    parse a Quadratic object from a string.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+use std::str::FromStr;
+use std::num::ParseFloatError;
+
 pub struct Quadratic {
     coeff_a: f64,
     coeff_b: f64,
@@ -23,14 +26,53 @@ impl Quadratic {
     }
 }
 
+impl FromStr for Quadratic {
+    type Err = ParseFloatError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let coeffs: Vec<&str> = s.trim_matches(|p| p == '(' || p == ')')
+                                    .trim()
+                                    .split(',')
+                                    .collect();
+
+        let a = coeffs[0].parse::<f64>()?;
+        let b = coeffs[1].parse::<f64>()?;
+        let c = coeffs[2].parse::<f64>()?;
+
+        Ok(Quadratic{ coeff_a: a, coeff_b: b, coeff_c: c })
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
+    use std::str::FromStr;
+    
     use crate::Quadratic;
 
     #[test]
     fn happy_case_1() {
         let q = Quadratic::new(-2f64, 2f64, 1f64);
+        let (x0, x1) = q.solve();
+        //println!("{}", x0);
+        //println!("{}", x1);
+        assert_eq!((x0.abs() - (-0.3660254f64).abs() < 0.000001), true);
+        assert_eq!((x1.abs() - (1.3660254f64).abs() < 0.000001), true);
+    }
+
+    #[test]
+    fn parse_string_happy_case_1() {
+        let q = Quadratic::from_str("-2.0,2.0,1.0").unwrap();
+        let (x0, x1) = q.solve();
+        //println!("{}", x0);
+        //println!("{}", x1);
+        assert_eq!((x0.abs() - (-0.3660254f64).abs() < 0.000001), true);
+        assert_eq!((x1.abs() - (1.3660254f64).abs() < 0.000001), true);
+    }
+
+    #[test]
+    fn parse_string_happy_case_2() {
+        let q = Quadratic::from_str("(-2.0,2.0,1.0)").unwrap();
         let (x0, x1) = q.solve();
         //println!("{}", x0);
         //println!("{}", x1);
